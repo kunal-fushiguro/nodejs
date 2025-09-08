c927c35f023f94a743fdf2ca1c018301f7b3ad0e
kunal-fushiguro <natwarlaluzumaki@gmail.com>
2025-09-08 22:03:45 +0530

    streams

--- 3_streams/index.ts
+++ 3_streams/index.ts
@@ -11,20 +11,20 @@
 //   console.timeEnd("start");
 // })();
 
-import fs from "fs";
+// import fs from "fs";
 
-(async () => {
-  console.time("start");
+// (async () => {
+//   console.time("start");
 
-  const write = fs.createWriteStream("text.txt", { encoding: "utf-8" });
+//   const write = fs.createWriteStream("text.txt", { encoding: "utf-8" });
 
-  for (let i = 0; i < 100000; i++) {
-    const buff = Buffer.from(` ${i}`, "utf-8");
-    write.write(buff);
-  }
-  write.end();
+//   for (let i = 0; i < 100000; i++) {
+//     const buff = Buffer.from(` ${i}`, "utf-8");
+//     write.write(buff);
+//   }
+//   write.end();
 
-  write.on("finish", () => {
-    console.timeEnd("start");
-  });
-})();
+//   write.on("finish", () => {
+//     console.timeEnd("start");
+//   });
+// })();
