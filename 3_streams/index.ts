eadb2063793d27455434bf7b3a44f44950893f43
kunal-fushiguro <natwarlaluzumaki@gmail.com>
2025-09-10 13:07:58 +0530

    streams

--- 3_streams/index.ts
+++ 3_streams/index.ts
@@ -1,30 +1,65 @@
-// import fs from "fs/promises";
+import path from "path";
+import fs from "fs";
+import ffmpegPath from "ffmpeg-static";
+import ffmpeg from "fluent-ffmpeg";
 
-// (async () => {
-//   console.time("start");
-//   const file = await fs.open("text.txt", "w");
+const args = process.argv.splice(2);
+const OUTPUTPATH = path.join(__dirname, "formarts");
+const INPUTPATH = path.join(__dirname);
 
-//   for (let i = 0; i < 100000; i++) {
-//     await file.write(` ${i}`);
-//   }
+async function checkVideoExist(path: string | undefined): Promise<boolean> {
+  try {
+    return fs.existsSync(path || "");
+  } catch {
+    return false;
+  }
+}
 
-//   console.timeEnd("start");
-// })();
+function checkffmpeg() {
+  if (ffmpegPath !== null) {
+    ffmpeg.setFfmpegPath(ffmpegPath);
+    return ffmpeg;
+  } else {
+    return false;
+  }
+}
 
-// import fs from "fs";
+async function main(): Promise<void> {
+  try {
+    const VIDEO_PATH = path.join(INPUTPATH, args[0]);
+    const validation = await checkVideoExist(VIDEO_PATH);
+    if (!validation) {
+      console.error(`${args[0]} video does not exist.`);
+      return;
+    }
+    const ffmpegRef = checkffmpeg();
+    if (ffmpegRef === false) {
+      console.error("ffmpeg is not installed");
+      return;
+    }
+    if (!fs.existsSync(OUTPUTPATH)) {
+      fs.mkdirSync(OUTPUTPATH, { recursive: true });
+    }
+    const VIDEO_OUTPUT_PATH = path.join(OUTPUTPATH, "output_360p.mp4");
 
-// (async () => {
-//   console.time("start");
+    ffmpegRef(VIDEO_PATH)
+      .videoCodec("libx264")
+      .audioCodec("aac")
+      .size("?x360")
+      .format("mp4")
+      .on("error", (e) => {
+        console.error("Error while Conversion:", e.message);
+      })
+      .on("end", () => {
+        console.log("Conversion Done");
+      })
+      .save(VIDEO_OUTPUT_PATH);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Unable to process the file");
+      console.error("Error:", error.message);
+    }
+  }
+}
 
-//   const write = fs.createWriteStream("text.txt", { encoding: "utf-8" });
-
-//   for (let i = 0; i < 100000; i++) {
-//     const buff = Buffer.from(` ${i}`, "utf-8");
-//     write.write(buff);
-//   }
-//   write.end();
-
-//   write.on("finish", () => {
-//     console.timeEnd("start");
-//   });
-// })();
+main();
